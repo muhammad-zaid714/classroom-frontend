62be1ec926d8f1cd7d96b72e17d44f0d99235d84
Zaid Awais <zaidawais58@gmail.com>
2026-05-07 15:20:17 +0500

    implement arcjet-secuirty

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,8 +1,23 @@
 import { BACKEND_BASE_URL } from "@/constants";
 import { ListResponse } from "@/types";
+import { HttpError } from "@refinedev/core";
 import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
 if(!BACKEND_BASE_URL){
   throw new Error("BACKEND_BASE_URL is not defined in environment variables.");
+}
+const buildHTTPError = async (response: Response):Promise<HttpError> => {
+  let message = "Request failed";
+  try {
+    const payload = (await response.json()) as {message?: string};
+    if(payload?.message) message = payload.message;
+
+  } catch (error) {
+    //ignore error
+  }
+  return {
+    message,
+    statusCode: response.status || 500,
+  }
 }
 const options: CreateDataProviderOptions = {
   getList:{
@@ -23,10 +38,12 @@ const options: CreateDataProviderOptions = {
       return params;
   },
     mapResponse:async(response)=>{
+      if(!response.ok) throw await buildHTTPError(response);
       const payload :ListResponse = await response.clone().json()
       return payload.data || [];
     },
     getTotalCount:async(response)=>{
+      if(!response.ok) throw await buildHTTPError(response);
       const payload :ListResponse = await response.json()
       return payload.pagination?.total || payload.data?.length || 0;
     }
